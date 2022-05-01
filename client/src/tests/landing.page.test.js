8415cea8009a895d10e79acd3648fb2683989e93
NadiiaSka <syvakivska@gmail.com>
2022-05-01 17:38:55 +1200

    Add a test case for Landing page

---
+++ client/src/tests/landing.page.test.js
@@ -0,0 +1,14 @@
+import { render, screen } from "@testing-library/react";
+import Landing from "../pages/Landing";
+import { BrowserRouter } from "react-router-dom";
+
+test("Login/Register button should be present on the screen", async () => {
+  render(
+    <BrowserRouter>
+      <Landing />
+    </BrowserRouter>
+  );
+  const loginButton = screen.getByText(/Login/i);
+
+  expect(loginButton).toBeInTheDocument();
+});
