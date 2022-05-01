385c9e52c0bc939f6d188ac6f92f9d97e8550341
NadiiaSka <syvakivska@gmail.com>
2022-05-01 15:51:27 +1200

    Impove test description

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders dashboard title text", () => {
+test("Renders dashboard title text", () => {
   render(<App />);
   const titleElement = screen.getByText(/Dashboard/i);
   expect(titleElement).toBeInTheDocument();
