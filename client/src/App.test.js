4b79b15fbfe0ce053ec9a71cf5cdbc2585b4a975
NadiiaSka <syvakivska@gmail.com>
2022-05-01 16:05:12 +1200

    trigger github actions

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("Renders a title text for the dashboard page", () => {
+test("Renders a title text for the dashboard", () => {
   render(<App />);
   const titleElement = screen.getByText(/Dashboard/i);
   expect(titleElement).toBeInTheDocument();
