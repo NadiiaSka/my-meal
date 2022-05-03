298ecac1ad4b0ae79f4963c4edd921913f1114b5
NadiiaSka <syvakivska@gmail.com>
2022-05-04 09:44:13 +1200

    improve error handling

--- backend/middleware/error-handler.js
+++ backend/middleware/error-handler.js
@@ -1,10 +1,19 @@
 const errorHandlerMiddleware = (err, req, res, next) => {
-  console.log(err);
+  console.log(err.message);
   const defaultError = {
     statusCode: 500,
-    msg: "Something went wrong, try again later",
+    msg: err.message || "Something went wrong, try again later",
   };
-  res.status(defaultError.statusCode).json({ msg: err });
+  if (err.name === "ValidationError") {
+    defaultError.statusCode = 400;
+    defaultError.msg = Object.values(err.errors).map((item) => item.message);
+  }
+  if (err.code && err.code === 11000) {
+    defaultError.statusCode = 400;
+    defaultError.msg = `${Object.keys(err.keyValue)} field has to be unique`;
+  }
+  //  res.status(defaultError.statusCode).json({ msg: err });
+  res.status(defaultError.statusCode).json({ msg: defaultError.msg });
 };
 
 export default errorHandlerMiddleware;
