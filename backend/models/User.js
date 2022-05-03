12c9503667131647f8c3129cd2e91329f5bec9bb
NadiiaSka <syvakivska@gmail.com>
2022-05-03 22:10:47 +1200

    add mongoose user model

---
+++ backend/models/User.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+import validator from "validator";
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please provide name"],
+    minlength: 3,
+    maxlength: 20,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide email"],
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: "Please provide a valid email",
+    },
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide password"],
+    minlength: 6,
+  },
+});
+
+export default mongoose.model("User", UserSchema);
