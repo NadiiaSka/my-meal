97220f3c849043fbd8ee8341f8d3cd2435b25650
NadiiaSka <syvakivska@gmail.com>
2022-05-03 20:22:50 +1200

    fix docker-compose issues

---
+++ backend/config/database.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const MONGO_PORT = process.env.MONGO_PORT || 27017;
+const MONGO_URL = process.env.MONGO_URL || `mongodb://localhost:${MONGO_PORT}`;
+let mongoUri = `${MONGO_URL}/mymeal`;
+let _connection;
+
+mongoose.connection.on("error", (e) => {
+  if (e.message.code === "ETIMEDOUT") {
+    console.error(e);
+    mongoose.connect(mongoUri);
+    return;
+  }
+  console.error(e);
+});
+
+mongoose.connection.once("open", (e) => {
+  console.info(`MongoDB successfully connected to ${mongoUri}`);
+});
+
+const connect = async (uriOverride) => {
+  // return stashed connection
+  if (_connection) {
+    return _connection;
+  }
+
+  // lazy instatiate
+  mongoUri = uriOverride || mongoUri;
+  _connection = await mongoose.connect(mongoUri);
+  return _connection;
+};
+
+const disconnect = async () => {
+  await mongoose.disconnect();
+};
+
+module.exports = { connect, disconnect };
